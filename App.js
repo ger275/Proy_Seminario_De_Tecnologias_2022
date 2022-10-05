a408b7ef9552973dbcf3084402001642af4dcf25
Gerson Lopez <gersondanielalbizures348@outlook.com>
2022-10-04 21:54:25 -0600

    Diseño interfaz login y consumir API para iniciar con cuenta de google

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, Button, TextInput } from 'react-native';
+import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg"
 
 // expo web: 884256678652-eetcjdolhpfhugi9996e38701oed1n79.apps.googleusercontent.com
 // android: 884256678652-r4hh05053ps31ukbrv1ilcf5868go8n9.apps.googleusercontent.com
@@ -14,14 +15,14 @@ export default function App() {
   const [accessToken, setAccessToken] = React.useState(null);
   const [user, setUser] = React.useState(null);
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    expoClientId: "884256678652-eetcjdolhpfhugi9996e38701oed1n79.apps.googleusercontent.com", 
+    expoClientId: "884256678652-eetcjdolhpfhugi9996e38701oed1n79.apps.googleusercontent.com",
     androidClientId: "884256678652-r4hh05053ps31ukbrv1ilcf5868go8n9.apps.googleusercontent.com",
     webClientId: "884256678652-4i9ba4og3chkvbkuu24s75npv1c4bhup.apps.googleusercontent.com"
   });
 
   // este ayuda para cuando el usuario inicia la aplicacion veamos si el usuario tienen una sesion o no 
   React.useEffect(() => {
-    if(response?.type === "success"){
+    if (response?.type === "success") {
       setAccessToken(response.authentication.accessToken);
       accessToken && fetchUserInfo();
     }
@@ -36,42 +37,69 @@ export default function App() {
   }
 
   const ShowUserInfo = () => {
-    if(user) {
+    if (user) {
       return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-          <Text style={{fontSize: 35, fontWeight: 'bold', marginBottom: 20}}>Bienvenido</Text>
-          <Image source={{uri: user.picture}} style={{width: 100, height: 100, borderRadius: 50}} />
-          <Text style={{fontSize: 20, fontWeight: 'bold'}}>{user.name}</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text style={{ fontSize: 35, fontWeight: 'bold', marginBottom: 20 }}>Bienvenido</Text>
+          <Image source={{ uri: user.picture }} style={{ width: 100, height: 100, borderRadius: 50 }} />
+          <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{user.name}</Text>
         </View>
       )
     }
   }
 
+  const SvgFondoBlanco = (props) => (
+    <Svg
+    width={350}
+    height={830}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <Path
+      d="M0 50C0 22.386 22.386 0 50 0h300v830H50c-27.614 0-50-22.386-50-50V50Z"
+      fill="#fff"
+    />
+  </Svg>
+  );
+
   return (
-    <View style={styles.container}>
-      {user && <ShowUserInfo />}
-      {user === null &&
+    <View style={styles.mainContainer}>
+      <View style={styles.containerSVG}>
+        <SvgFondoBlanco />
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>BIENVENIDO</Text>
+        <Text style={styles.subTitulo}>Login</Text>
+        <TextInput
+          placeholder="Email"
+          style={styles.textInput}
+        />
+        <TextInput
+          placeholder="Contraseña"
+          style={styles.textInput}
+        />
+
+        {user && <ShowUserInfo />}
+        {user === null &&
           <>
-          <Text style={{fontSize: 35, fontWeight: 'bold'}}>Bienvenido!</Text>
-          <Text style={{fontSize: 25, fontWeight: 'bold', marginBottom: 20, color: 'gray'}}>Inicia sesión</Text>
-        <TouchableOpacity
-          disabled={!request}
-          onPress={() => {
-            promptAsync();
-            }} 
-        >
-          <Text>Usuario</Text>
-          <Text>Contraseña</Text>
+            <TouchableOpacity
+              disabled={!request}
+              onPress={() => {
+                promptAsync();
+              }}
+            >
 
-          <Button
-          title={accessToken ? "Get User Data" : "Inicia con Google"}
-          onPress={accessToken ? getUserData : () => { promptAsync({useProxy: false, showInRecents: true}) }}
-          />
-          <StatusBar style="auto" />
+              <Button
+                title={accessToken ? "Get User Data" : "Inicia con Google"}
+                onPress={accessToken ? getUserData : () => { promptAsync({ useProxy: false, showInRecents: true }) }}
+              />
+              <StatusBar style="auto" />
 
-        </TouchableOpacity>
-        </>
-      }
+            </TouchableOpacity>
+          </>
+        }
+      </View>
     </View>
   );
 }
@@ -79,10 +107,48 @@ export default function App() {
 // <Image source={require("./login.png")} style={{width: 300, height: 40}} />
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  mainContainer: {
+    backgroundColor: 'red',
     alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,
+    zIndex: 1,
+    position: 'relative'
   },
+  container: {
+    marginTop: '50%',
+    zIndex: 3,
+    position: 'absolute',
+    backgroundColor: '#FFFFFF'
+  },
+  containerSVG: {
+    alignItems: 'flex-end',
+    width: '100%',
+    height: '80%',
+    zIndex: 2,
+    position: 'absolute',
+    marginTop: 0
+  },
+  titulo: {
+    fontSize: 40,
+    color: '#FB8C00',
+    fontWeight: 'bold'
+  },
+  subTitulo: {
+    fontSize: 20,
+    color: '#FB8C00',
+    marginTop: 130,
+    marginBottom: 40,
+    fontWeight: 'bold'
+  },
+  textInput: {
+    padding: 10,
+    paddingStart: 30,
+    borderColor: '#FDD835',
+    borderWidth: 3,
+    width: '100%',
+    height: 50,
+    marginBottom: 20,
+    borderTopLeftRadius: 15,
+    borderBottomRightRadius: 15
+  }
 });
